29d688227fabeedc27e3d5611a09f6aa2eaffa27
阿•波茨德额佛 <101253821+a-bcdef@users.noreply.github.com>
2024-08-28 15:12:48 +0800

    Update and rename 酷我.js to tubiao.json

--- 酷我.js
+++
@@ -1,6 +0,0 @@
-[SCRIPT]
-# KuWo 解锁VIP脚本
-KuWo_VIP = type=http-response, pattern=^(?!.*img).*?kuwo\.cn(/vip|/openapi)?(/enc|/audi.tion|/v[\d]/(user/vip\?(vers|apiVersion|platform|op\=ui|_t)|theme\?op=gd|sysinfo\?op=getRePayAndDoPayBoxNew|api(/pay)?/((user/personal/)?user/info|payInfo/kwplayer/payMiniBar|advert/(myPage|iListen|album))|album/(adBar|myRec/vipMusic))|/kuwopay/vip-tab/setting|/(audioApi/)?a\.p($|\?op\=getvip|.*?ptype\=vip)|/mobi\.s\?f\=kwxs|/music\.pay\?newver\=3$|/(EcomResource|(Mobile)?Ad)Serv(er|ice)), requires-body=true, max-size=0, script-path=https://napi.ltd/script/Worker/KuWo.js
-
-[MITM]
-hostname = *.kuwo.cn
